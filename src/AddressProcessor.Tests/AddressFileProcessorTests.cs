26a9cc75b3eb1fa95293605510a1cc94ca57a8cc
Simon Duff <simon.j.duff@gmail.com>
2018-02-01 10:48:49 +0530

    Ported test into dotnet core

---
+++ src/AddressProcessor.Tests/AddressFileProcessorTests.cs
@@ -0,0 +1,36 @@
+﻿using AddressProcessing.Address;
+using AddressProcessing.Address.v1;
+using Xunit;
+
+namespace AddressProcessing.Tests
+{
+    public class AddressFileProcessorTests
+    {
+        private FakeMailShotService _fakeMailShotService;
+        private const string TestInputFile = @"test_data\contacts.csv";
+
+        public AddressFileProcessorTests()
+        {
+            _fakeMailShotService = new FakeMailShotService();
+        }
+
+        [Fact]
+        public void Should_send_mail_using_mailshot_service()
+        {
+            var processor = new AddressFileProcessor(_fakeMailShotService);
+            processor.Process(TestInputFile);
+
+            Assert.Equal(229, _fakeMailShotService.Counter);
+        }
+
+        internal class FakeMailShotService : IMailShot
+        {
+            internal int Counter { get; private set; }
+
+            public void SendMailShot(string name, string address)
+            {
+                Counter++;
+            }
+        }
+    }
+}
